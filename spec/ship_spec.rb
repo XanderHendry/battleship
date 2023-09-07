d72a089255f200531ed619dbaff880e9a89b2d95
Xander Hendry <vhendry.coding@gmail.com>
2023-09-07 18:04:08 -0400

    Add hit test, and one sunk test

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe Ship do
       expect(ship.health).to eq(ship.length)
     end
   end
+  
+  describe '#hit' do
+    it 'remove a single point of health from a ship' do
+      expect(ship.health).to eq(3)
+      ship.hit
+      expect(ship.health).to eq(2)
+    end
+  end
+
+  describe '#sunk?' do
+    it 'will default as false' do
+      expect(ship.sunk?).to eq(false)
+    end
+
+  end
 end
