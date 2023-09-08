12b40414682d1ca6adb2012371cab987ca0b73c9
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-09-07 21:57:32 -0600

    Write test for Board #initialize

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,6 +7,20 @@ RSpec.describe Board do
   describe '#initialize' do
     it 'can initialize' do
       expect(@board).to be_a(Board)
+      expect(@board.cells).to be_a(Hash)
+      expect(@board.cells.length).to eq(16)
+      @board.cells.keys.each do |cell_name|
+        expect(cell_name).to be_a(String)
+      end
+      @board.cells.values.each do |cell|
+        expect(cell).to be_a(Cell)
+      end
     end
   end
+
+  describe '#valid_coordinate?' do
+
+  end
+
+  
 end
