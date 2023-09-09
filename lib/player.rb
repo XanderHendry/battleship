58ac489902ddd87f4cf01ecee4e0feb19014a09f
Xander Hendry <vhendry.coding@gmail.com>
2023-09-09 16:23:55 -0400

    Clean up unused code

--- lib/player.rb
+++ lib/player.rb
@@ -12,10 +12,6 @@ class Player
   def place(name, coordinates)
     @board.place(@ships[name], coordinates)
   end 
-  # def place(name, coordinates)
-  #   ship = ships.find { |ship| ship.name == name.capitalize}
-  #   @board.place(ship, coordinates)
-  # end
 
   def render_board
     @board.render(true)
