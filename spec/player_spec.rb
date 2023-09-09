0a70c94735c4b185b92c1ab12d18c502ff043000
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-09-08 23:25:23 -0600

    Add spec test for Player #initialize

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,7 +5,13 @@ RSpec.describe Player do
   let(:player) { Player.new } 
   describe '#initialize' do
     it 'can initialize' do
-
+      expect(player).to be_a(Player)
+      expect(player.board).to be_a(Board)
+      expect(player.fireable_cells).to eq(player.board.keys)
+      expect(player.ships).to be_a(Array)
+      players.ships.each do |ship|
+        expect(ship).to be_a(Ship)
+      end
     end
   end
 
