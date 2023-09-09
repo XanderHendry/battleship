c83cdf96eb5d49e325abbf18bfabfe6802694e81
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-09-09 16:50:44 -0600

    Add human player portion of setup

--- lib/game.rb
+++ lib/game.rb
@@ -18,7 +18,23 @@ class Game
   end
 
   def setup
-    puts "Thank you"
-    gets
+    puts "I have laid out my ships on the grid."
+    puts "You now need to lay out your two ships."
+    puts "The Cruiser is three units long and the Submarine is two units long."
+    puts player1.render_board
+    puts "Enter the squares for the Cruiser (3 spaces):"
+    coordinates = gets.chomp.split(" ")
+    until player1.board.valid_placement?(player1.ships[0], coordinates)
+      puts "Those are invalid coordinates. Please try again"
+      coordinates = gets.chomp.split(" ")
+    end
+    player1.place(player1.ships[0], coordinates)
+    puts "Enter the squares for the Submarine (2 spaces):"
+    coordinates = gets.chomp.split(" ")
+    until player1.board.valid_placement?(player1.ships[1], coordinates)
+      puts "Those are invalid coordinates. Please try again"
+      coordinates = gets.chomp.split(" ")
+    end
+    player1.place(player1.ships[1], coordinates)
   end
 end
