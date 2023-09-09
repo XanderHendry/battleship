8ce86a0695d79bbe8d6bef06e7565cd3804dd438
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-09-08 23:16:40 -0600

    Set up testing skeleton

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,5 +2,22 @@ require 'spec_helper'
 require 'pry'
 
 RSpec.describe Player do
+  let(:player) { Player.new } 
+  describe '#initialize' do
+    it 'can initialize' do
 
+    end
+  end
+
+  describe '#render' do
+    it 'will render board with ships displayed' do
+
+    end
+  end
+
+  describe '#take_turn' do
+    it 'can take a turn' do
+
+    end
+  end
 end
