d752e1d39bfbfb02cda9d709b1d60586ac0e8631
Xander Hendry <vhendry.coding@gmail.com>
2023-09-09 16:20:38 -0400

    Impliment #render_board by passing tests

--- lib/player.rb
+++ lib/player.rb
@@ -5,12 +5,19 @@ class Player
   def initialize
     @board = Board.new
     @fireable_cells = @board.keys
-    @ships = [cruiser = Ship.new("Cruiser", 3), submarine = Ship.new("Submarine", 2)]
+    @ships = {cruiser: Ship.new("Cruiser", 3),
+     submarine: Ship.new("Submarine", 2)}
   end
 
   def place(name, coordinates)
-    ship = ships.find { |ship| ship.name == name.capitalize}
-    @board.place(ship, coordinates)
-  end
+    @board.place(@ships[name], coordinates)
+  end 
+  # def place(name, coordinates)
+  #   ship = ships.find { |ship| ship.name == name.capitalize}
+  #   @board.place(ship, coordinates)
+  # end
 
+  def render_board
+    @board.render(true)
+  end
 end
