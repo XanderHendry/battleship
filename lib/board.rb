5f69cb079b26168abcb77582cb3b5dc10438a5b8
Xander Hendry <vhendry.coding@gmail.com>
2023-09-11 16:25:41 -0400

    Add progress to place_ships refactor. edit Player, Board, and Human

--- lib/board.rb
+++ lib/board.rb
@@ -2,24 +2,6 @@ class Board
   attr_reader :cells
   def initialize(length = 4, width = 4)
     @cells = cell_maker(length, width)
-    # @cells = {
-    #   "A1" => Cell.new("A1"),
-    #   "A2" => Cell.new("A2"),
-    #   "A3" => Cell.new("A3"),
-    #   "A4" => Cell.new("A4"),
-    #   "B1" => Cell.new("B1"),
-    #   "B2" => Cell.new("B2"),
-    #   "B3" => Cell.new("B3"),
-    #   "B4" => Cell.new("B4"),
-    #   "C1" => Cell.new("C1"),
-    #   "C2" => Cell.new("C2"),
-    #   "C3" => Cell.new("C3"),
-    #   "C4" => Cell.new("C4"),
-    #   "D1" => Cell.new("D1"),
-    #   "D2" => Cell.new("D2"),
-    #   "D3" => Cell.new("D3"),
-    #   "D4" => Cell.new("D4")
-    # }
   end
 
   def grid_maker(length, width)
@@ -54,7 +36,8 @@ class Board
     valid_length?(ship, coordinates) && 
     consecutive?(ship, coordinates) && 
     not_diagonal?(ship, coordinates) && 
-    all_vacant?(ship, coordinates)
+    all_vacant?(ship, coordinates) &&
+    coordinates.all? { |coordinate| valid_coordinate?(coordinate) }
   end
 
   def valid_length?(ship, coordinates)
--- lib/human.rb
+++ lib/human.rb
@@ -1,4 +1,5 @@
 require './lib/player.rb'
+require 'pry'
 
 class Human < Player
 
@@ -6,7 +7,23 @@ class Human < Player
     @board.render(true)
   end
 
-  def place(ship, coordinates)
-    super
+  def place
+    @ships.each do |ship|
+      puts "Enter the squares for the #{ship.name} (#{ship.length} spaces)"
+      coordinates = gets.chomp.split(" ")
+      until @board.valid_placement?(ship, coordinates) || coordinates == "quit"
+        puts "That is not a valid placement, please try again!"
+        coordinates = gets.chomp
+      end
+      super(ship, coordinates)
+      puts "==============PLAYER BOARD=============="
+      puts render_board
+      puts "#{ship.name} placed, press enter to move on!"
+      gets
+    end
+    puts "==============PLAYER BOARD=============="
+    puts render_board
+    puts "All ships placed! Press enter to start game!"
+    gets
   end
 end
